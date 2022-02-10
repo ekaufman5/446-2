c0790c09b39f613d0cd6e962ae59758305513226
emma <ekaufman@u.northwestern.edu>
2022-02-10 12:58:24 -0600

    wow wrong order

--- equations.py
+++ equations.py
@@ -10,8 +10,8 @@ class KdVEquation:
         self.u = u
         self.dudx = spectral.Field(domain, dtype=self.dtype)
         self.ududx = spectral.Field(domain, dtype=self.dtype)
-        x_basis = x_basis = spectral.Fourier(self.N)
-        self.N = len(domain)
+        N = len(domain)
+        x_basis = x_basis = spectral.Fourier(self.N)        
         self.kx = x_basis.wavenumbers(u.dtype)        
         #N = x_basis.N
         #kx = x_basis.wavenumbers()
